36f86b3d34200fd4b927d951b4618ddca5d33ac2
PabloCruz201120 <klabitos@usal.es>
2021-11-15 10:57:16 +0100

    [ADD][Flappy] movimiento absoluto del cuadrado

--- Flappy/script/script.js
+++ Flappy/script/script.js
@@ -1,21 +1,56 @@
-document.body.addEventListener("keydown", conClickTeclado);
+document.body.addEventListener("keydown", comprobanteTecla);
+var cuadradoQueSeMueve = document.getElementById("cuadradoPrueba"); //77 top //45 left centro
 
+function comprobanteTecla(evento){
+    conClickTeclado(evento);
+    comprobarMovimiento(evento);
+}
+
+function comprobarMovimiento(evento){
+    switch(evento.key){
+        case "w":
+            movimientoArriba(evento);
+            break;
+        case "a":
+            movimientoIzq(evento);
+            break;
+        case "s":
+            movimientoAbajo(evento);
+            break;
+        case "d":
+            movimientoDerecha(evento);
+            break;
+    }
+}
 
 function conClickTeclado(evento){
     let pLog = document.getElementById("keyLog");
     pLog.innerText=`TECLA: ${evento.key} y CÓDIGO: ${evento.keyCode}`;
 }
 
-function movimientoIzq(){
+function movimientoIzq(evento){
+    var topVal = parseInt(window.getComputedStyle(cuadradoQueSeMueve, null).getPropertyValue("left"),10);
+    cuadradoQueSeMueve.style.left = (topVal - 100) + "px";
+    
+    
+}
+
+function movimientoDerecha(evento){
+    
+    var topVal = parseInt(window.getComputedStyle(cuadradoQueSeMueve, null).getPropertyValue("left"),10);
+    cuadradoQueSeMueve.style.left = (topVal + 100) + "px";
 
 }
 
-function movimientoDerecha(){
+function movimientoArriba(evento){
+    
+    var topVal = parseInt(window.getComputedStyle(cuadradoQueSeMueve, null).getPropertyValue("top"),10);
+    cuadradoQueSeMueve.style.top = (topVal - 100) + "px";
 
 }
 
-function movimientoArriba(){
+function movimientoAbajo(evento){
 
+    var topVal = parseInt(window.getComputedStyle(cuadradoQueSeMueve, null).getPropertyValue("top"),10);
+    cuadradoQueSeMueve.style.top = (topVal + 100) + "px";
 }
-
-function movimientoAbajo(){}
